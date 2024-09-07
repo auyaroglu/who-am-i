8e01f32f06692d49cfc2d7150f1faa0206d04c5d
auyaroglu <ardauyaroglu@hotmail.com>
2024-09-07 20:56:43 +0300

    bug fixes

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@ import { withPayload } from "@payloadcms/next/withPayload"
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Your Next.js config here
+    reactStrictMode: true,
+    swcMinify: true,
 }
 
 export default withPayload(nextConfig)
