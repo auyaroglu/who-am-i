3f896bbab920effe00fc09d69b1c4fea4af3fb1f
auyaroglu <ardauyaroglu@hotmail.com>
2024-09-26 17:19:08 +0300

    favicon eklendi

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -2,12 +2,19 @@ import { ReactNode } from "react"
 import { cn } from "@/app/lib/utils"
 import { Inter as FontSans } from "next/font/google"
 import { Toaster } from "@/app/components/ui/toaster"
+import type { Metadata } from "next"
 
 type LayoutProps = {
     children: ReactNode
 }
 
 import "./globals.css"
+
+export const metadata: Metadata = {
+    icons: {
+        icon: "/favicon.ico",
+    },
+}
 
 const fontSans = FontSans({
     subsets: ["latin"],
